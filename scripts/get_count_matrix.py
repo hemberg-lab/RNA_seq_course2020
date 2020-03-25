eb27b74c8b19449475b46a61be55e7a5c96ae98e
geparada <geparada88@gmail.com>
2020-03-25 23:21:31 +0000

    Update get_count_matrix.py

--- scripts/get_count_matrix.py
+++ scripts/get_count_matrix.py
@@ -1,4 +1,36 @@
 import csv, sys
+from collections import defaultdict
+
+def main(count_list):
+	
+	gene_count = defaultdict(list)
+	
+	for file in count_list:
+		
+		
+		with open(file) as F:
+			reader = csv.DictReader(filter(lambda row: row[0]!='#', F), delimiter="\t")
+			
+			for row in reader:
+				
+				samples = []
+				
+				for key in row.keys():
+					
+					if 'hisat2' in key:
+							
+						sample = key.split("/")[1].split(".")[0]
+						samples.append( sample )
+							
+
+				
+	
+	
+	
+	
+	
+
+
 
 if __name__ == '__main__':
-	main(sys.argv[1])
+	main(sys.argv[1:])
