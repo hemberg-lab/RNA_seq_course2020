3539e21f154bcdfd1dfac567bab7c83175bd3c7b
geparada <geparada88@gmail.com>
2020-03-26 00:03:59 +0000

    Update get_count_matrix.py

--- scripts/get_count_matrix.py
+++ scripts/get_count_matrix.py
@@ -16,21 +16,26 @@ def main(count_list):
 				samples = []
 				
 				for key in row.keys():
-					
 					if 'hisat2' in key:
-							
-						sample = key.split("/")[1].split(".")[0]
-						samples.append( sample )
-							
-
-				
+						samples.append( key )
+						
+			sample_counts = [(x.split("/")[1].split(".")[0], row[x]) for x in samples]
+			
+			for i in sample_counts:
+				 gene_count[row["Geneid"]].append(i)
+			
+			
+	sample_IDs = [x[0] for x in gene_count[list(gene_count.keys())[0]]]
 	
+	header = ["gene"] + sample_IDs
+	print( "\t".join(header) )
 	
+	for gene, sample_count_list in  gene_count.items():
+		
+		counts =  [x[1] for x in sample_count_list]
+		out = [gene] + counts
+		print( "\t".join(out) )
 	
-	
-	
-
-
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
