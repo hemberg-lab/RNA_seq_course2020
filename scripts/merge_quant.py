3b273f0b54d34b85172a3b1b3b53c410479312f2
geparada <geparada88@gmail.com>
2020-10-23 11:40:00 +0100

    Update merge_quant.py

--- scripts/merge_quant.py
+++ scripts/merge_quant.py
@@ -8,23 +8,24 @@ csv.field_size_limit()
 
 
 def main(mode, out_file, file_list  ):
+    with open(out_file, 'w') as out:
 
-    for file in file_list:
+        for file in file_list:
 
-        with gzip.open(file, mode="rt") as f, open(out_file, 'a') as out:
+            with gzip.open(file, mode="rt") as f:
 
-            header = ["Sample", mode, "TpM", "Read_Counts"]
+                header = ["Sample", mode, "TpM", "Read_Counts"]
 
-            writer = csv.DictWriter(out, fieldnames=header, extrasaction='ignore', delimiter="\t")
-            writer.writeheader()
+                writer = csv.DictWriter(out, fieldnames=header, extrasaction='ignore', delimiter="\t")
+                writer.writeheader()
 
-            sample = file.split("/")[-1].split(".")[0]
-            reader = csv.DictReader(f, delimiter="\t")
+                sample = file.split("/")[-1].split(".")[0]
+                reader = csv.DictReader(f, delimiter="\t")
 
-            for row in reader:
+                for row in reader:
 
-                row["Sample"] = sample
-                writer.writerow(row)
+                    row["Sample"] = sample
+                    writer.writerow(row)
                 
 if __name__ == '__main__':
     main(sys.argv[1], sys.argv[2], sys.argv[3:])
