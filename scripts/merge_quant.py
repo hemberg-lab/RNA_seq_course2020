545a319dd8e52b53b4101d37a75ef2b7bc4e0150
geparada <geparada88@gmail.com>
2020-11-02 23:45:22 +0000

    Update merge_quant.py

--- scripts/merge_quant.py
+++ scripts/merge_quant.py
@@ -11,28 +11,43 @@ def main(mode, out_file, file_list  ):
     with open(out_file, 'w') as out:
 
         for file in file_list:
+            
 
-            with gzip.open(file, mode="rt") as f:
+            if mode=="Isoform" or mode=="Gene":
+                header = ["Sample", mode, "TpM", "Read_Counts"]
+
+                with gzip.open(file, mode="rt") as f:
+
+
+                    writer = csv.DictWriter(out, fieldnames=header, extrasaction='ignore', delimiter="\t")
+                    writer.writeheader()
+
+                    sample = file.split("/")[-1].split(".")[0]
+                    reader = csv.DictReader(f, delimiter="\t")
+
+                    for row in reader:
+
+                        row["Sample"] = sample
+                        writer.writerow(row)
                 
-                if mode=="Isoform" or mode=="Gene":
-                    header = ["Sample", mode, "TpM", "Read_Counts"]
-                elif mode=="Isoform_salmon":
-                    header = ["Sample", 'Name', 'Length', 'EffectiveLength', 'TPM', 'NumReads']
+                
+            elif mode=="Isoform_salmon":
+                header = ["Sample", 'Name', 'Length', 'EffectiveLength', 'TPM', 'NumReads']
+                
+                with open(file) as f:
 
-                writer = csv.DictWriter(out, fieldnames=header, extrasaction='ignore', delimiter="\t")
-                writer.writeheader()
+                    writer = csv.DictWriter(out, fieldnames=header, extrasaction='ignore', delimiter="\t")
+                    writer.writeheader()
 
-                sample = file.split("/")[-1].split(".")[0]
-                reader = csv.DictReader(f, delimiter="\t")
+                    sample = file.split("/")[-1].split(".")[0]
+                    reader = csv.DictReader(f, delimiter="\t")
 
-                for row in reader:
+                    for row in reader:
 
-                    row["Sample"] = sample
-                    writer.writerow(row)
+                        row["Sample"] = sample
+                        writer.writerow(row)
                 
 if __name__ == '__main__':
     main(snakemake.params["feature"], snakemake.output["merged"],  snakemake.input["files"])
 
-        
-        
-    
+
